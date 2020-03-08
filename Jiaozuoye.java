d4b96e9ce410679ca11c86e22daa0079d797a6e2
lunhui-li <61857921+lunhui-li@users.noreply.github.com>
2020-03-08 17:11:49 +0800

    akka

---
+++ Jiaozuoye.java
@@ -0,0 +1,15 @@
+package work;
+
+public interface Jiaozuoye extends A,B {
+     String Xin ="Happy!";
+     float day=12.5f;
+     abstract void le();
+}
+ interface A{
+    abstract void ye();
+ }
+ interface B{
+    abstract void c();
+ }
+
+
